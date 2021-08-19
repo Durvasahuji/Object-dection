e80069e2f57a2fe74acaf751a232f3a272ec3a7f
Durvasahuji <74409509+Durvasahuji@users.noreply.github.com>
2021-08-19 20:56:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+img = "";
+
+function preload(){
+    img = loadImage('bed_room.jpg');
+    img = loadImage('tv.jpg');
+    img = loadImage('lm.jpg');
+    img = loadImage('bottle.jpg');
+    img = loadImage('fruit.jpg');
+}
+}
+  }
+
+  function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    
+  }
+
+  function draw()
+   {
+ 
+    fill(255, 0, 0);
+    text("BED") 40 , 36;
+    noFill();
+    stroke(255, 0, 0);
+    rect(160, 55 , 240, 255);
+  }
+
+  
